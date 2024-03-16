a715ec18d9e1f8ee796459490f3d91c793a8373c
prudence18 <prudenceabong@gmail.com>
2024-03-16 18:53:36 -0400

    created vpc

---
+++ eks_vpc/variable.tf
@@ -0,0 +1,93 @@
+variable "networking" {
+  description = "Networking configuration for the VPC"
+  type = object({
+    cidr_block      = string
+    region          = string
+    vpc_name        = string
+    azs             = list(string)
+    public_subnets  = list(string)
+    private_subnets = list(string)
+    nat_gateways    = bool
+  })
+  default = {
+    cidr_block      = "10.0.0.0/16"
+    region          = "us-east-1"
+    vpc_name        = "custom-vpc"
+    azs             = ["us-east-1a", "us-east-1b"]
+    public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+    private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+    nat_gateways    = true
+  }
+}
+
+variable "egress_rules" {
+  description = "Egress rules for the security group"
+  type        = list(object({
+    description      = string
+    from_port        = number
+    to_port          = number
+    protocol         = string
+    cidr_blocks      = list(string)
+    ipv6_cidr_blocks = list(string)
+  }))
+  default = [
+    {
+      description      = "Allow all outbound traffic"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  ]
+}
+
+variable "ingress_rules" {
+  description = "Ingress rules for the security group"
+  type        = list(object({
+    description      = string
+    from_port        = number
+    to_port          = number
+    protocol         = string
+    cidr_blocks      = list(string)
+    ipv6_cidr_blocks = list(string)
+  }))
+  default = [
+    {
+      description      = "Allow HTTPS"
+      from_port        = 443
+      to_port          = 443
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+    },
+    {
+      description      = "Allow HTTP"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+    }
+  ]
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+}
